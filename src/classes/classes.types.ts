1f5271deabc1587d4a0ddab244b095664c8fae20
Bystrova Ann <ann.bystrova96@mail.ru>
2023-11-29 04:51:43 +0300

    feat: do append by flag

--- src/classes/classes.types.ts
+++ src/classes/classes.types.ts
@@ -15,7 +15,7 @@ export type TStyle<C extends TClassesKeys> = Record<C, CSSRule>;
 export type TStyleFn<C extends TClassesKeys, P extends TProps> = ((props: P) => TStyle<C>) | TStyle<C>;
 
 export type TClasses<C extends TClassesKeys> = Record<C, TClassesValues>;
-export type TUseClasses<C extends string, P> = (props: P) => TClasses<C>;
+export type TUseClasses<C extends string, P> = (props: P, doAppend?: boolean) => TClasses<C>;
 export type TMakeClasses<T extends TTheme> = <C extends TClassesKeys, P extends TProps = undefined>(
   style: TStyleFn<C, P & { theme: T }>,
   options?: Partial<TOptions>,
